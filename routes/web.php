55a050425593bcd595a563044885bf20fc585845
夏赵建 <xzj8023tp@163.com>
2019-01-23 08:19:40 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+//上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
